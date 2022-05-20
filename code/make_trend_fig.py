bd29cbc707809c776bf08ccf139c30f671b57f33
Erin Sheldon <erin.sheldon@gmail.com>
2022-05-20 08:27:38 -0400

    update no cells

--- code/make_trend_fig.py
+++ code/make_trend_fig.py
@@ -75,6 +75,7 @@ def do_Tratio_bias_plot(ax):
     )
 
     Tratios = [1.2, 1.3, 1.4, 1.5]
+
     # run-riz-drcbWP-cells
     mlows = np.array(
         [0.000227885, -0.000161471,  -0.000193378, -0.000423672]
@@ -82,7 +83,6 @@ def do_Tratio_bias_plot(ax):
     mhighs = np.array(
         [0.00148068, 0.00118696, 0.00143503, 0.00158595]
     )/0.001
-    # add run-drcbWP here
 
     ax.fill_between(
         Tratios,
@@ -90,15 +90,35 @@ def do_Tratio_bias_plot(ax):
         mhighs,
         color=FCOLOR,
         alpha=ALPHA,
+        hatch=HATCHES[0],
+        label='cells',
+    )
+
+    # run-drcbWP
+    mlows = np.array(
+        [-0.000375975, -0.000637519, -0.000838113, -0.00100003]
+    )/0.001
+    mhighs = np.array(
+        [0.000483453, 0.000308397, 0.000336744, 0.000378551],
+    )/0.001
+
+    ax.fill_between(
+        Tratios,
+        mlows,
+        mhighs,
+        color=FCOLOR,
+        alpha=ALPHA,
+        hatch=HATCHES[1],
+        label='no cells',
     )
 
     add_lines(ax)
 
-    ax.text(
-        1.2,
-        -2,
-        r'99.7\% confidence errors',
-    )
+    # ax.text(
+    #     1.2,
+    #     -2,
+    #     r'99.7\% confidence errors',
+    # )
 
 
 def do_s2n_bias_plot(ax):
@@ -128,12 +148,12 @@ def do_s2n_bias_plot(ax):
         label='cells',
     )
 
-    # add run-drcbWP here
+    # run-drcbWP
     mlows = np.array(
-        [-0.00052429, -0.000516172, -0.000374928, -0.000414037],
+        [-0.000506131, -0.000547303, -0.000375975, -0.000404532]
     )/0.001
     mhighs = np.array(
-        [0.000572595, 0.000440756, 0.0005201, 0.000372421],
+        [0.000519134, 0.000391566, 0.000483453, 0.000371225],
     )/0.001
     ax.fill_between(
         s2ns,
@@ -151,7 +171,7 @@ def do_s2n_bias_plot(ax):
     ax.text(
         10,
         -2,
-        r'99.7\% confidence errors',
+        r'99.7\% confidence range',
     )
     ax.legend()
 
