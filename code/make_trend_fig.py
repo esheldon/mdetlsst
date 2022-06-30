9c1f73700a1b2915014a45acc505a198042d6167
Erin Sheldon <erin.sheldon@gmail.com>
2022-06-30 11:05:41 -0400

    incorporate latest results

--- code/make_trend_fig.py
+++ code/make_trend_fig.py
@@ -82,12 +82,21 @@ def do_Tratio_bias_plot(ax):
     Tratios = [1.2, 1.3, 1.4, 1.5]
 
     # run-riz-drcbWP-cells
+    # mlows = np.array(
+    #     [0.000227885, -0.000161471,  -0.000193378, -0.000423672]
+    # )/0.001
+    # mhighs = np.array(
+    #     [0.00148068, 0.00118696, 0.00143503, 0.00158595]
+    # )/0.001
+
+    # run-WPv0.1-nowarp
     mlows = np.array(
-        [0.000227885, -0.000161471,  -0.000193378, -0.000423672]
+        [0.000211992, 0.000151222, -5.32513e-05, -0.000346328]
     )/0.001
     mhighs = np.array(
-        [0.00148068, 0.00118696, 0.00143503, 0.00158595]
+        [0.000967327, 0.00104341, 0.000960636, 0.000968678]
     )/0.001
+
 
     ax.fill_between(
         Tratios,
@@ -96,26 +105,28 @@ def do_Tratio_bias_plot(ax):
         color=COLORS[0],
         alpha=ALPHA,
         hatch=HATCHES[0],
-        label='cells',
+        # label='cells',
+        label='LSST 10 year PSF',
     )
 
     # run-drcbWP
-    mlows = np.array(
-        [-0.000375975, -0.000637519, -0.000838113, -0.00100003]
-    )/0.001
-    mhighs = np.array(
-        [0.000483453, 0.000308397, 0.000336744, 0.000378551],
-    )/0.001
+    # mlows = np.array(
+    #     [-0.000375975, -0.000637519, -0.000838113, -0.00100003]
+    # )/0.001
+    # mhighs = np.array(
+    #     [0.000483453, 0.000308397, 0.000336744, 0.000378551],
+    # )/0.001
+    #
+    # ax.fill_between(
+    #     Tratios,
+    #     mlows,
+    #     mhighs,
+    #     color=COLORS[1],
+    #     alpha=ALPHA,
+    #     hatch=HATCHES[1],
+    #     label='no cells',
+    # )
 
-    ax.fill_between(
-        Tratios,
-        mlows,
-        mhighs,
-        color=COLORS[1],
-        alpha=ALPHA,
-        hatch=HATCHES[1],
-        label='no cells',
-    )
     # run-WM-nowarp
     mlows = np.array(
         [0.000165007, -8.56819e-05, -0.000114263, -0.000370727]
@@ -153,11 +164,19 @@ def do_s2n_bias_plot(ax):
 
     s2ns = [10, 12.5, 15, 20]
     # run-riz-drcbWP-cells
+    # mlows = np.array(
+    #     [0.000553132, -5.80659e-05, 0.000227885, -3.67881e-05]
+    # )/0.001
+    # mhighs = np.array(
+    #     [0.0020267, 0.00131326, 0.00148068, 0.00113388]
+    # )/0.001
+
+    # run-WPv0.1-nowarp
     mlows = np.array(
-        [0.000553132, -5.80659e-05, 0.000227885, -3.67881e-05]
+        [0.000219245, 7.80083e-05, 0.000213062, 0.000257441]
     )/0.001
     mhighs = np.array(
-        [0.0020267, 0.00131326, 0.00148068, 0.00113388]
+        [0.00121502, 0.000868853, 0.000972641, 0.000891671]
     )/0.001
 
     ax.fill_between(
@@ -167,26 +186,27 @@ def do_s2n_bias_plot(ax):
         color=COLORS[0],
         alpha=ALPHA,
         hatch=HATCHES[0],
-        label='cells',
+        # label='cells',
+        label='LSST 10 year PSF',
     )
 
     # run-drcbWP
-    mlows = np.array(
-        [-0.000506131, -0.000547303, -0.000375975, -0.000404532]
-    )/0.001
-    mhighs = np.array(
-        [0.000519134, 0.000391566, 0.000483453, 0.000371225],
-    )/0.001
-    ax.fill_between(
-        s2ns,
-        mlows,
-        mhighs,
-        # color=FCOLOR,
-        color=COLORS[1],
-        alpha=ALPHA,
-        hatch=HATCHES[1],
-        label='no cells',
-    )
+    # mlows = np.array(
+    #     [-0.000506131, -0.000547303, -0.000375975, -0.000404532]
+    # )/0.001
+    # mhighs = np.array(
+    #     [0.000519134, 0.000391566, 0.000483453, 0.000371225],
+    # )/0.001
+    # ax.fill_between(
+    #     s2ns,
+    #     mlows,
+    #     mhighs,
+    #     # color=FCOLOR,
+    #     color=COLORS[1],
+    #     alpha=ALPHA,
+    #     hatch=HATCHES[1],
+    #     label='no cells',
+    # )
 
     # run-WM-nowarp
     mlows = np.array(
@@ -205,8 +225,6 @@ def do_s2n_bias_plot(ax):
         hatch=HATCHES[2],
         label='constant PSF',
     )
-
-
 
     add_lines(ax)
 
